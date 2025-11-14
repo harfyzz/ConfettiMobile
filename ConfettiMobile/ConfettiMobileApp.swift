e4c0913f69cc1dbd8308fa5b5ed1869b0e8aab6f
Afeez <harfyzz@gmail.com>
2025-11-14 21:41:08 +0000

    Initial Commit

---
+++ ConfettiMobile/ConfettiMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConfettiMobileApp.swift
+//  ConfettiMobile
+//
+//  Created by Afeez Yunus on 14/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct ConfettiMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
